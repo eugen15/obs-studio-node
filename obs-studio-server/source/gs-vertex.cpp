7dae38bd5579a1e2f304d7f2ab58d8f9e953165a
Eugen <97041180+eugen15@users.noreply.github.com>
2023-03-29 16:57:44 -0400

    test4

--- obs-studio-server/source/gs-vertex.cpp
+++ obs-studio-server/source/gs-vertex.cpp
@@ -28,6 +28,9 @@ GS::Vertex::Vertex()
 
 
 
+
+
+
 	this->hasStore = true;
 	this->store = util::malloc_aligned(16, sizeof(vec3) * 3 + sizeof(uint32_t) + sizeof(vec4) * MAXIMUM_UVW_LAYERS);
 	this->position = reinterpret_cast<vec3 *>(store);
